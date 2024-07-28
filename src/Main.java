b88a8f280c423e5d574e1c7a58d4787f5f6adf25
Nadine273 <tomns@mail.ru>
2024-07-28 18:07:56 +0200

    homework is done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Task 1");
+        List<Integer> nums1 = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        for (int i = 0; i < nums1.size(); i++) {
+            if (nums1.get(i) % 2 == 1){
+                System.out.print(nums1.get(i)+", ");
+            }
+        }
+        System.out.println(" ");
+
+        System.out.println("Task 2");
+        List<Integer> nums2 = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        List<Integer> evenNumbers= new ArrayList<>();
+        for (int i = 0; i < nums2.size(); i++) {
+            if (nums2.get(i) % 2 == 0 && !evenNumbers.contains(nums2.get(i))){
+                evenNumbers.add(nums2.get(i));
+            }
+        }
+        Collections.sort(evenNumbers);
+        System.out.println(evenNumbers.toString());
+
+        System.out.println("Task 3");
+        List<String> words = new ArrayList<>(List.of("cat","dog","mouse","dog","cow","cat"));
+        Set<String> animals = new HashSet<>();
+        for (int i = 0; i < words.size(); i++) {
+        animals.add(words.get(i));
+        }
+        System.out.println(animals.toString());
+
+        System.out.println("Task 4");
+        List<String> strings = new ArrayList<>(List.of("один", "два","два", "три", "три", "три"));
+        Map<String, Integer> result = strings.stream().collect(Collectors.toMap(
+                        e -> e,
+                        e -> 1,
+                        Integer::sum));
+        System.out.println(result.toString());
+
+
 
     }
 }
